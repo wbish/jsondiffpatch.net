5005f5ca73cf3a3ba8530fe979a735653c237b98
William Bishop <xation@gmail.com>
2016-02-05 19:27:11 -0800

    Version tweaks

--- Src/Properties/AssemblyInfo.cs
+++ Src/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("JsonDiffPatchDotNet")]
+[assembly: AssemblyTitle("JsonDiffPatch.Net")]
 [assembly: AssemblyDescription("Json object diffs and patching (jsondiffpatch compatible)")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("William Bishop")]
-[assembly: AssemblyProduct("JsonDiffPatchDotNet")]
+[assembly: AssemblyProduct("JsonDiffPatch.Net")]
 [assembly: AssemblyCopyright("Copyright © William Bishop 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyVersion("1.0.2.0")]
+[assembly: AssemblyFileVersion("1.0.2.0")]
