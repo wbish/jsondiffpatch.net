0c1c7adf9f8aafe8423669bddfa5eea262c36eae
William Bishop <xation@gmail.com>
2018-03-03 13:29:49 -0800

    VS2017 Builds
    Use Visual Studio 2017 format. Conslidate all sln/csproj into single
    version.

--- Build/build.bat
+++ Build/build.bat
@@ -1,6 +1,4 @@
 @ECHO OFF
 
-"%ProgramFiles(x86)%\MSBuild\14.0\Bin\msbuild.exe" /p:Configuration=Debug ..\Src\JsonDiffPatchDotNet\JsonDiffPatchDotNet.csproj
-"%ProgramFiles(x86)%\MSBuild\14.0\Bin\msbuild.exe" /p:Configuration=Release ..\Src\JsonDiffPatchDotNet\JsonDiffPatchDotNet.csproj
-"%ProgramFiles(x86)%\MSBuild\14.0\Bin\msbuild.exe" /p:Configuration=Debug ..\Src\JsonDiffPatchDotNet\JsonDiffPatchDotNet.Portable45.csproj
-"%ProgramFiles(x86)%\MSBuild\14.0\Bin\msbuild.exe" /p:Configuration=Release ..\Src\JsonDiffPatchDotNet\JsonDiffPatchDotNet.Portable45.csproj
+"%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\msbuild.exe" /p:Configuration=Debug ..\Src\JsonDiffPatchDotNet\JsonDiffPatchDotNet.csproj
+"%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\msbuild.exe" /p:Configuration=Release ..\Src\JsonDiffPatchDotNet\JsonDiffPatchDotNet.csproj
