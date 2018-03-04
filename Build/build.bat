ded89300b5e8d1415d15b98ebce619aaaad933da
William Bishop <xation@gmail.com>
2018-03-03 19:07:30 -0800

    Remove Build/Tools dir
    No longer needed with new build system

--- Build/build.bat
+++
@@ -1,4 +0,0 @@
-@ECHO OFF
-
-"%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\msbuild.exe" /p:Configuration=Debug ..\Src\JsonDiffPatchDotNet\JsonDiffPatchDotNet.csproj
-"%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\msbuild.exe" /p:Configuration=Release ..\Src\JsonDiffPatchDotNet\JsonDiffPatchDotNet.csproj
